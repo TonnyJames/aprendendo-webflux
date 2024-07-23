a38aa69511e073387fcfcb461d5e7e0ce804ef74
tonnyReis <tonny.jreis@outlook.com>
2024-07-23 20:36:04 -0300

    adcionado titulo do projeto nas configs

---
+++ src/main/java/br/com/tonnyreis/aprendendowebflux/AprendendoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package br.com.tonnyreis.aprendendowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tonnyreis/aprendendowebflux/AprendendoWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tonnyreis.aprendendowebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
