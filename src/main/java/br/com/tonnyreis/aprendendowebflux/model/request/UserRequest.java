c3763f9ec94cbd35afc53cf240cfafd70f454e3f
tonnyReis <tonny.jreis@outlook.com>
2024-11-16 12:15:59 -0300

    Adicionando @anotation própria para espaços em branco em uma string.

--- src/main/java/br/com/tonnyreis/aprendendowebflux/model/request/UserRequest.java
+++ src/main/java/br/com/tonnyreis/aprendendowebflux/model/request/UserRequest.java
@@ -1,19 +1,23 @@
 package br.com.tonnyreis.aprendendowebflux.model.request;
 
+import br.com.tonnyreis.aprendendowebflux.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+        @TrimString
         @Size(min = 3, max = 50, message = "must have a minimum size of 3 and maximum of 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
 
+        @TrimString
         @Email(message = "invalid email")
         @NotBlank(message = "must not be null or empty")
         String email,
 
+        @TrimString
         @Size(min = 3, max = 20, message = "must have a minimum size of 3 and maximum of 20 characters")
         @NotBlank(message = "must not be null or empty")
         String password
