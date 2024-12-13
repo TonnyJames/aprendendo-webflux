55a5ce051467d514fb4da9d1c24112f7f251b81e
tonnyReis <tonny.jreis@outlook.com>
2024-12-13 00:15:47 -0300

    corrigindo construtor para classe ObjectNotFoundException.java

--- src/main/java/br/com/tonnyreis/aprendendowebflux/service/exception/ObjectNotFoundException.java
+++ src/main/java/br/com/tonnyreis/aprendendowebflux/service/exception/ObjectNotFoundException.java
@@ -3,6 +3,6 @@ package br.com.tonnyreis.aprendendowebflux.service.exception;
 public class ObjectNotFoundException extends RuntimeException {
 
     public ObjectNotFoundException(String message) {
-
+        super(message);
     }
 }
